01b132280540a1890fa49ad191e83bb66151b0ff
DanielDeveloper <danielfelipedeveloper@gmail.com>
2020-10-24 14:44:25 -0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import './database/connect';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(5000, () => console.log('Server is Running at http://localhost:5000'));
